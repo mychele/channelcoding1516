5cc45b0d01b1a68524009305aa91f5bcd1881403
mychele <michele.polese@gmail.com>
2015-11-07 18:46:13 +0100

    Ex 2 and 5 for HW1, MATLAB code

---
+++ cc_hw1_e2.m
@@ -0,0 +1,34 @@
+sigma_w2 = 1;
+xmax = 100;
+x = -xmax:xmax; % support (received signal)
+x_plus = 0:xmax;
+x_min = -xmax:0;
+R = 1;
+Pbit = qfunc(sqrt(0.5/sigma_w2*R));
+
+%% AWGN, soft decoding
+f_awgnsd = 2*x/sigma_w2;
+
+%% AWGN, hard decoding
+f_awgnhd_plus = log((1-Pbit)/Pbit)*ones(1, length(x_plus));
+f_awgnhd_minus = -log((1-Pbit)/Pbit)*ones(1, length(x_min)-1);
+f_awgnhd = [f_awgnhd_minus, f_awgnhd_plus];
+
+
+%% Laplacian
+f_lapl = -sqrt(2)/sqrt(sigma_w2)*(abs(x-1) - abs(x+1));
+
+%% MoG
+p_x1 = sqrt(5)/(8*sqrt(2*pi*sigma_w2))*(4/sqrt(2)*exp(-5*(x-1).^2/(4*sigma_w2)) + exp(-5*(x-1).^2/(8*sigma_w2)) + ...
+	 1/sqrt(8)*exp(-5*(x-1).^2/(16*sigma_w2)) + 1/4*exp(-5*(x-1).^2/(32*sigma_w2)));
+p_x0 = sqrt(5)/(8*sqrt(2*pi*sigma_w2))*(4/sqrt(2)*exp(-5*(x+1).^2/(4*sigma_w2)) + exp(-5*(x+1).^2/(8*sigma_w2)) + ...
+	 1/sqrt(8)*exp(-5*(x+1).^2/(16*sigma_w2)) + 1/4*exp(-5*(x+1).^2/(32*sigma_w2)));
+f_mog = log(p_x1./p_x0);
+
+%% plot
+figure,
+plot(x, f_awgnsd, x, f_lapl, x, f_mog), hold on
+stairs(x, f_awgnhd)
+xlabel('x')
+ylabel('LLR')
+legend('AWGN, SD', 'LAPLACE', 'MoG', 'AWGN, HD')
---
+++ cc_hw1_e5.m
@@ -0,0 +1,55 @@
+disp('This is the given pcc matrix H = [B, C]perm')
+H = [ 1 0 1 1 1 0 0 0 1 1;
+	  0 1 0 1 1 1 1 0 0 1;
+	  0 1 0 0 1 1 1 1 0 1;
+	  1 1 1 1 0 0 0 1 1 0;
+	  1 0 1 0 0 1 1 1 1 0; ];
+disp(H);
+% notice that it is full rank;
+disp(strcat('Rank of H is ', num2str(rank(H))));
+
+% build the permutation matrix
+perm_matrix = eye(max(size(H)));
+% in this case
+row4 = perm_matrix(4, :);
+row5 = perm_matrix(5, :);
+row6 = perm_matrix(6, :);
+perm_matrix(4, :) = row5;
+perm_matrix(5, :) = row6;
+perm_matrix(6, :) = row4;
+disp('Permutation matrix');
+disp(perm_matrix);
+
+disp('This is Htilde=[B, C]=H*transpose(perm) with C invertible')
+Htilde_nonstd = H*perm_matrix';
+disp(Htilde_nonstd);
+
+disp('Perform some row operations on Htilde to get C^{-1}')
+Hb = sum_rows([eye(5), Htilde_nonstd], 1, 2)
+Hb = sum_rows(Hb, 2, 3)
+Hb = sum_rows(Hb, 1, 4)
+Hb = sum_rows(Hb, 3, 4)
+Hb = sum_rows(Hb, 3, 5)
+Hb = sum_rows(Hb, 2, 5)
+Hb = sum_rows(Hb, 4, 5)
+Hb = sum_rows(Hb, 5, 3)
+Hb = sum_rows(Hb, 5, 1)
+Hb = sum_rows(Hb, 4, 3)
+Hb = sum_rows(Hb, 4, 2)
+Hb = sum_rows(Hb, 4, 1)
+
+A = Hb(:, 6:10)
+C_1 = Hb(:, 1:5)
+Htilde = Hb(:, 6:end)
+
+Gtilde = [eye(5); A];
+disp('This is the Gp generating matrix')
+disp(Gtilde);
+mod(Htilde*Gtilde, 2)
+mod(Htilde_nonstd*Gtilde, 2)
+
+disp('This is the G generating matrix')
+G = perm_matrix'*Gtilde;
+disp(G);
+disp('and this is the result of H*G')
+mod(H*G, 2)
