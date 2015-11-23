3f5e98bb79631723c50976ef60e86e603d952f04
mychele <michele.polese@gmail.com>
2015-11-23 19:38:12 +0100

    Refined ex 7

--- HW1/dmin_finder.m
+++ HW1/dmin_finder.m
@@ -1,4 +1,4 @@
-N = 4; % codeword length
+N = 2; % codeword length
 mem = 2; % memory length (size of the state vector)
 M = 2; % binary constellation
 num_states = M^mem;
@@ -11,20 +11,28 @@ state = zeros(mem, 1); % state vector of size mem, with lsb in the left most pos
 state_ID = 0:3; % state ID (i.e. 00 -> 0, 01 -> 1...)
 
 % g3
-% input_vec_mult = [1; 0; 1];
-% state_mat_mult = [0 1; 0 1; 1 1];
+input_vec_mult = [1; 0; 1];
+state_mat_mult = [0 1; 0 1; 1 1];
 
 % g4
-input_vec_mult = [1; 0; 1];
-state_mat_mult = [0 0; 0 1; 1 0];
+% input_vec_mult = [1; 0; 1];
+% state_mat_mult = [0 0; 0 1; 1 0];
 
 % (5, 7)
 % input_vec_mult = [1; 1];
 % state_mat_mult = [0 1; 1 1];
 
 % (5, 7, 7)
-input_vec_mult = [1; 1; 1; 1];
-state_mat_mult = [0 1; 1 1; 1 1; 1 1];
+% input_vec_mult = [1; 1; 1];
+% state_mat_mult = [0 1; 1 1; 1 1];
+
+% state_ID, symbol
+%neigh_ext = [0, 0; 1, 1; 2, 0; 3, 1; 0, 1; 1, 0; 2, 1; 3, 0]; % g4
+%neigh_ext = [0, 0; 1, 0; 2, 0; 3, 0; 0, 1; 1, 1; 2, 1; 3, 1]; % g3 but also (5,7)
+neigh_ext = [0, 0; 1, 1; 2, 1; 3, 0; 0, 1; 1, 0; 2, 0; 3, 1];
+
+input_vec_mult = [1; 1];
+state_mat_mult = [0 0; 1 0];
 
 for state_ID = 0:num_states-1
 	% update state vector
@@ -38,74 +46,26 @@ for state_ID = 0:num_states-1
 	end
 end
 
-% Neighbors vector
-% neigh = zeros(num_states, mem);
-% for state_ID = 0:num_states-1
-% 	switch mod(state_ID, 2)
-% 		case 0
-% 			neigh(state_ID + 1, :) = [0, 1];
-% 		case 1
-% 			neigh(state_ID + 1, :) = [2, 3];
-% 			
-% 	end
-% end
-
-% state_ID, symbol
-%neigh_ext = [0, 0; 1, 1; 2, 0; 3, 1; 0, 1; 1, 0; 2, 1; 3, 0]; % g4
-neigh_ext = [0, 0; 1, 0; 2, 0; 3, 0; 0, 1; 1, 1; 2, 1; 3, 1]; % g3 but also (5,7)
-
-
 % Cycle on the received vector r
 % Init
 Gamma_prev = inf*ones(num_states, 1);
 Gamma_prev(1) = 0; % start from state 0
 Gamma = inf*ones(num_states, 1);
 
-while (Gamma(1) == inf)
-	% cycle through states
-	for state_ID = 0:num_states-1
-		% compute the input symbol that can bring to this state
-		% for 00 and 01 the input symbol that bring to the state is always
-		% 0, for 10 and 11 the input symbol is 1
-		u_poss = floor(state_ID/mem);
-		
-		% cycle on the neighbors, compute new cost, update if lower than
-		% before
-		cost = inf;
-		for i = 1:mem
-			neigh_ID = neigh_ext(state_ID*mem + i, 1);
-			u_poss = neigh_ext(state_ID*mem + i, 2);
-			newcost = Gamma_prev(neigh_ID+1) + sum(y_lut(u_poss*N + 1:(u_poss + 1)*N, neigh_ID+1));
-			if (newcost < cost) % new min
-				if(~(state_ID==0 && neigh_ID == 0 && newcost == 0)) % avoid to account for the path from 0 to 0
-					cost = newcost;
-				end
-				Gamma(state_ID+1) = cost;
-			end
-		end
-		if (cost == inf) % it was not initialized
-			Gamma(state_ID+1) = cost;
-		end
-	end
-	Gamma_prev = Gamma;
- 	Gamma_prev(1) = inf;
-end
-Gamma_zero(1) = Gamma(1);
 for l = 1:20
 	% cycle through states
 	for state_ID = 0:num_states-1
-		% compute the input symbol that can bring to this state
-		% for 00 and 01 the input symbol that bring to the state is always
-		% 0, for 10 and 11 the input symbol is 1
-		u_poss = floor(state_ID/mem);
-		
 		% cycle on the neighbors, compute new cost, update if lower than
 		% before
 		cost = inf;
 		for i = 1:mem
 			neigh_ID = neigh_ext(state_ID*mem + i, 1);
 			u_poss = neigh_ext(state_ID*mem + i, 2);
-			newcost = Gamma_prev(neigh_ID+1) + sum(y_lut(u_poss*N + 1:(u_poss + 1)*N, neigh_ID+1));
+			if (neigh_ID == 0 && u_poss == 0)
+				newcost = inf; % invalidate transition from 0 to 0
+			else
+				newcost = Gamma_prev(neigh_ID+1) + sum(y_lut(u_poss*N + 1:(u_poss + 1)*N, neigh_ID+1));
+			end
 			if (newcost < cost) % new min
 				if(~(state_ID==0 && neigh_ID == 0 && newcost == 0)) % avoid to account for the path from 0 to 0
 					cost = newcost;
@@ -118,8 +78,6 @@ for l = 1:20
 		end
 	end
 	Gamma_prev = Gamma;
-	Gamma_zero(l+1) = Gamma(1);
- 	Gamma_prev(1) = inf;
+ 	Gamma_zero(l) = Gamma(1);
 end
-
 disp(min(Gamma_zero))
