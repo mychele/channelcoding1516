88f19a8bd947c72e6bc4d3c7101151fc7c4fcbbb
mychele <michele.polese@gmail.com>
2016-02-18 20:25:44 +0100

    Script for blade

---
+++ final_project_DWDM/blade_sim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#
+# Output in current folder
+#
+#$ -cwd
+#
+# Send me an email when job ends or it is aborted
+#
+#$ -m ea
+#
+# Specify parallel queue
+# 
+# and set the number of parallel jobs
+# no
+
+./launch_blade.sh
