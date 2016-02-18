6f86bb16857bf6fe6f81fc2f5abc30f890f2c770
mychele <michele.polese@gmail.com>
2016-02-18 16:21:11 +0100

    Corrected the logic of the CheckNode, which now has a vector of LLR as private variable

--- final_project_DWDM/classes/check_node.cpp
+++ final_project_DWDM/classes/check_node.cpp
@@ -4,45 +4,99 @@
 
 CheckNode::CheckNode() {
 	m_line = line(-1, -1);
+	m_llrVector = new std::vector<double>();
+	m_llrVector->resize(ALL_ROWS);
 }
 
 CheckNode::CheckNode(line lineEq) {
 	m_line = lineEq;
+	m_llrVector = new std::vector<double>();
+	m_llrVector->resize(ALL_ROWS);
 }
 
 void 
 CheckNode::updateLLR(std::vector<VariableNode> *variableNodeVector) {
-	// cycle on the variableNodes of the line, compute sum(phiTilde(|x|)) and prod(sgn(x))
+	// cycle on all the outgoing branch, one per line, and update the LLR of each one
+	for(int row_index = 0; row_index < ALL_ROWS; ++row_index) {
+		//std::cout << "Updating LLR for outgoing branch " << row_index << "\n";
+		updateLLRat(row_index, variableNodeVector);
+	}
+}
+
+void
+CheckNode::updateLLRat(int row_index, std::vector<VariableNode> *variableNodeVector) {
+	// cycle on the other incoming branches, compute phiTilde(sum(phiTilde(|x|)) and prod(sgn(x)))
+
+	// verbose mode for debugging
+	bool verb = 0; //(m_line.first==0 && m_line.second==29);
+
+	// variables
 	double sumPhiTilde = 0;
 	int prodSgn = 1;
 	int slope = slopes[m_line.first];
 	int b;
 	int variable_node_index;
 	double llr_var;
-	for (int a = 0; a < (int)ALL_ROWS; ++a) {
+
+	// cycle on all the rows, with the exception of row_index
+	for (int a = 0; a < row_index; ++a) {
 					//(a*s + c)%293
 		b = (a*slope + m_line.second)%ALL_COLUMNS;
 		variable_node_index = ALL_COLUMNS*a + b;
 		llr_var = variableNodeVector->at(variable_node_index).getLLR();
+		
+		if(verb) {std::cout << "LLR of node " << a << "," << b << " = " << llr_var << " for check_node " << m_line.first << " " << m_line.second <<"\n";}
+		
 		if(llr_var > 0) {
 			sumPhiTilde += phiTilde(llr_var);
 		} else if(llr_var < 0) {
 			sumPhiTilde += phiTilde(-llr_var);
-			prodSgn = -prodSgn;
+			prodSgn = - prodSgn;
 		} else { // llr_var is equal to 0. Then phiTilde(llr_var) -> inf, and also the sum is inf. Then phiTilde(inf) = 0
 			sumPhiTilde = std::numeric_limits<double>::infinity(); //just a flag now
 			break; // no need to compute other variable nodes
 		}
+		if(verb) {std::cout << "sumPhiTilde = " << sumPhiTilde <<"\n";}
+		if(verb) {std::cout << "prodSgn = " << prodSgn <<"\n";}
 	}
-	if(isinf(sumPhiTilde)) {
-		m_llr = 0;
-	} else {
-		if(prodSgn > 0) {
-			m_llr = phiTilde(sumPhiTilde);
+
+	if(isinf(sumPhiTilde)) { // don't need to cycle also on the other rows, the sum will be surely +inf, and phiTilde(+inf)=0
+		// save the outgoing llr
+		m_llrVector->at(row_index) = 0;
+	} else { // continue to cycle
+		for (int a = row_index + 1; a < ALL_ROWS; ++a) {
+
+			b = (a*slope + m_line.second)%ALL_COLUMNS;
+			variable_node_index = ALL_COLUMNS*a + b;
+			llr_var = variableNodeVector->at(variable_node_index).getLLR();
+			
+			if(verb) {std::cout << "LLR of node " << a << "," << b << " = " << llr_var << " for check_node " << m_line.first << " " << m_line.second <<"\n";}
+			
+			if(llr_var > 0) {
+				sumPhiTilde += phiTilde(llr_var);
+			} else if(llr_var < 0) {
+				sumPhiTilde += phiTilde(-llr_var);
+				prodSgn = - prodSgn;
+			} else { // llr_var is equal to 0. Then phiTilde(llr_var) -> inf, and also the sum is inf. Then phiTilde(inf) = 0
+				sumPhiTilde = std::numeric_limits<double>::infinity(); //just a flag now
+				break; // no need to compute other variable nodes
+			}
+			if(verb) {std::cout << "sumPhiTilde = " << sumPhiTilde <<"\n";}
+			if(verb) {std::cout << "prodSgn = " << prodSgn <<"\n";}
+		}
+
+		// compute the outgoing llr
+		if(isinf(sumPhiTilde)) {
+			m_llrVector->at(row_index) = 0;
 		} else {
-			m_llr = - phiTilde(sumPhiTilde);
+			if(prodSgn > 0) {
+				m_llrVector->at(row_index) = phiTilde(sumPhiTilde);
+			} else {
+				m_llrVector->at(row_index) = - phiTilde(sumPhiTilde);
+			}
 		}
 	}
+	if(verb) {std::cout << "for outgoing branch " << row_index << " of check_node " << m_line.first << " " << m_line.second << " llr " << m_llrVector->at(row_index) <<"\n";}
 }
 
 void 
@@ -51,13 +105,23 @@ CheckNode::setLine(line lineEq) {
 }
 
 void 
-CheckNode::setLLR(double llr) {
-	m_llr = llr;
+CheckNode::setAllLLR(std::vector<double> *llrVector) {
+	m_llrVector = llrVector;
+}
+
+void
+CheckNode::setLLRat(double llr, int row_index) {
+	m_llrVector->at(row_index) = llr;
 }
 
 double 
-CheckNode::getLLR() const {
-	return m_llr;
+CheckNode::getLLRat(int row_index) const {
+	return m_llrVector->at(row_index);
+}
+
+const std::vector<double>*
+CheckNode::getAllLLR() const {
+	return m_llrVector;
 }
 
 line 
--- final_project_DWDM/classes/check_node.h
+++ final_project_DWDM/classes/check_node.h
@@ -17,16 +17,34 @@ public:
 
 	void setLine(line lineEq);
 
-	void setLLR(double llr);
+	/**
+	 * Set the LLR of the row_index outgoing branch
+	 */
+	void setLLRat(double llr, int row_index);
 
-	double getLLR() const;
+	void setAllLLR(std::vector<double> *llrVector);
+
+	/**
+	 * Get the complete vector of LLRs
+	 */
+	const std::vector<double>* getAllLLR() const;
+
+	/**
+	 * Get the LLR of row_index outgoing branch
+	 */
+	double getLLRat(int row_index) const;
 
 	line getLine() const;
 	
 private:
 	line m_line; // if m_line has both entries negative, the check node is one of the invalid ones
 	// TODO consider if it makes any difference to precompute the variable nodes and store them in an array or compute them on the fly
-	double m_llr;
+	/**
+	 * Vector that stores all the LLR, one entry per outgoing branch
+	 */
+	std::vector<double> *m_llrVector;
+
+	void updateLLRat(int row_index, std::vector<VariableNode> *variableNodeVector);
 
 
 };
