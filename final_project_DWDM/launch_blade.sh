fdeb251f7a1689e69ed2ca35e5a0d28b5ccc8573
mychele <michele.polese@gmail.com>
2016-02-29 19:19:18 +0100

    Fixed launch_blade.sh

--- final_project_DWDM/launch_blade.sh
+++ final_project_DWDM/launch_blade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-g++ -std=c++11 -O3 -c -I./classes -g ./classes/variable_node.cpp
-g++ -std=c++11 -O3 -c -I./classes -g ./classes/check_node.cpp
-g++ -std=c++11 -O3 -c -I./classes -g ./classes/ldpc_decoder.cpp
-g++ -std=c++11 -O3 -c -I./classes -g ./test/simulation_multi.cpp
-g++ -std=c++11 -O3 -o simulation_multi simulation_multi.o ldpc_decoder.o variable_node.o check_node.o
+g++ -std=c++11 -pthread -O3 -c -I./classes -g ./classes/variable_node.cpp
+g++ -std=c++11 -pthread -O3 -c -I./classes -g ./classes/check_node.cpp
+g++ -std=c++11 -pthread -O3 -c -I./classes -g ./classes/ldpc_decoder.cpp
+g++ -std=c++11 -pthread -O3 -c -I./classes -g ./test/simulation_multi.cpp
+g++ -std=c++11 -pthread -O3 -o simulation_multi simulation_multi.o ldpc_decoder.o variable_node.o check_node.o 
 
 ./simulation_multi
