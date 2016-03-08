c3ae45417821708e73684427d7e26e49cee1ef05
mychele <michele.polese@gmail.com>
2016-03-08 20:07:18 +0100

    Files with simulation results

--- final_project_DWDM/results/analyze_results.m
+++ final_project_DWDM/results/analyze_results.m
@@ -7,16 +7,17 @@ Pbit_uncoded = @(x) qfunc(sqrt(2*x));
 PER_uncoded = @(x) 1 - (1 - Pbit_uncoded(x)).^K;
 
 %% Read simulation_results_multi files for SumProduct
-ebno_dB_sumproduct = [2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.25, 6.5, 6.6, 6.7:0.05:7.1];
-gamma_sumproduct = 10.^(ebno_dB_sumproduct/10)*K/N;
+ebno_dB_sumproduct = [3, 3.5, 4, 4.2, 4.6, 4.7:0.02:4.94];
+gamma_sumproduct = 10.^(ebno_dB_sumproduct/10)*K/N*2;
 gamma_dB_sumproduct = 10*log10(gamma_sumproduct);
 num_err_packet_sp = zeros(length(ebno_dB_sumproduct), 1);
 num_err_bit_sp = zeros(length(ebno_dB_sumproduct), 1);
 num_attempt_per_snr_sp = zeros(length(ebno_dB_sumproduct), 1);
 
 for i = 1:length(ebno_dB_sumproduct)
-    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/simulation_results_multi_', num2str(ebno_dB_sumproduct(i)), '.txt'), 'r');
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/blade50/simulation_results_multi_', num2str(ebno_dB_sumproduct(i)), '.txt'), 'r');
     vec = fscanf(fileID, '%d');
+    fclose(fileID);
     num_attempt_per_snr_sp(i) = length(vec);
     num_err_packet_sp(i) = length(vec(vec~=0));
     num_err_bit_sp(i) = sum(vec(vec~=0));
@@ -24,10 +25,180 @@ end
 
 BER_sp = num_err_bit_sp./num_attempt_per_snr_sp;
 BER_sp = BER_sp./K;
-
+PER_sp_fromBER = 1 - (1 - BER_sp).^K;
 PER_sp = num_err_packet_sp./num_attempt_per_snr_sp;
 
-ebno_dB_minsum = [2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.25, 6.5, 6.6, 6.7:0.05:7.15];
+figure,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([2, 10])
+ylim([5e-8, 1e-1])
+grid on
+xlabel('Gamma [dB]')
+ylabel('BER')
+legend('LDPC', 'Uncoded')
+
+figure,
+semilogy(ebno_dB_sumproduct, PER_sp, '-^'), hold on,
+xlim([2, 7])
+ylim([5e-6, 1.1])
+grid on
+xlabel('EBNO [dB]')
+ylabel('PER')
+legend('50 iterations')
+
+%% Read simulation results for 1 iteration
+ebno_dB_1sp = [2,3,4,4.5,5,5.5,6,6.5,7];
+gamma_1sp = 10.^(ebno_dB_1sp/10)*K/N;
+gamma_dB_1sp = 10*log10(gamma_1sp);
+num_err_packet_1sp = zeros(length(ebno_dB_1sp), 1);
+num_err_bit_1sp = zeros(length(ebno_dB_1sp), 1);
+num_attempt_per_snr_1sp = zeros(length(ebno_dB_1sp), 1);
+
+for i = 1:length(ebno_dB_1sp)
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/blade1/simulation_results_multi_1_', num2str(ebno_dB_1sp(i)), '.txt'), 'r');
+    vec = fscanf(fileID, '%d');
+    fclose(fileID);
+    num_attempt_per_snr_1sp(i) = length(vec);
+    num_err_packet_1sp(i) = length(vec(vec~=0));
+    num_err_bit_1sp(i) = sum(vec(vec~=0));
+end
+
+BER_1sp = num_err_bit_1sp./num_attempt_per_snr_1sp;
+BER_1sp = BER_1sp./K;
+PER_1sp_fromBER = 1 - (1 - BER_1sp).^K;
+
+figure,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_1sp, BER_1sp, '-*'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([2, 7])
+ylim([1e-8, 1e-1])
+grid on
+xlabel('Gamma [dB]')
+ylabel('BER')
+legend('50', '1', 'Uncoded')
+
+%% Read results for 10 iterations
+ebno_dB_10sp = [2,3,4,4.5,4.6,4.7,4.8,4.9,5,5.1,6];
+gamma_10sp = 10.^(ebno_dB_10sp/10)*K/N;
+gamma_dB_10sp = 10*log10(gamma_10sp);
+num_err_packet_10sp = zeros(length(ebno_dB_10sp), 1);
+num_err_bit_10sp = zeros(length(ebno_dB_10sp), 1);
+num_attempt_per_snr_10sp = zeros(length(ebno_dB_10sp), 1);
+
+for i = 1:length(ebno_dB_10sp)
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/blade10/simulation_results_multi_10_', num2str(ebno_dB_10sp(i)), '.txt'), 'r');
+    vec = fscanf(fileID, '%d');
+    fclose(fileID);
+    num_attempt_per_snr_10sp(i) = length(vec);
+    num_err_packet_10sp(i) = length(vec(vec~=0));
+    num_err_bit_10sp(i) = sum(vec(vec~=0));
+end
+
+BER_10sp = num_err_bit_10sp./num_attempt_per_snr_10sp;
+BER_10sp = BER_10sp./K;
+PER_10sp_fromBER = 1 - (1 - BER_10sp).^K;
+
+figure,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_10sp, BER_10sp, '-*'), hold on,
+semilogy(ebno_dB_1sp, BER_1sp, '-o'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([2, 7])
+ylim([1e-8, 1e-1])
+grid on
+xlabel('Gamma [dB]')
+ylabel('BER')
+legend('50', '10', '1', 'Uncoded')
+
+%% Read results for 20 iterations
+ebno_dB_20sp = [3,4,4.2,4.5,4.6,4.7,4.8,4.9, 4.94, 5];
+gamma_20sp = 10.^(ebno_dB_20sp/10)*K/N;
+gamma_dB_20sp = 10*log10(gamma_20sp);
+num_err_packet_20sp = zeros(length(ebno_dB_20sp), 1);
+num_err_bit_20sp = zeros(length(ebno_dB_20sp), 1);
+num_attempt_per_snr_20sp = zeros(length(ebno_dB_20sp), 1);
+
+for i = 1:length(ebno_dB_20sp)
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/blade20/simulation_results_multi_20_', num2str(ebno_dB_20sp(i)), '.txt'), 'r');
+    vec = fscanf(fileID, '%d');
+    fclose(fileID);
+    num_attempt_per_snr_20sp(i) = length(vec);
+    num_err_packet_20sp(i) = length(vec(vec~=0));
+    num_err_bit_20sp(i) = sum(vec(vec~=0));
+end
+
+BER_20sp = num_err_bit_20sp./num_attempt_per_snr_20sp;
+BER_20sp = BER_20sp./K;
+PER_20sp_fromBER = 1 - (1 - BER_20sp).^K;
+
+figure,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_20sp, BER_20sp, '-d'), hold on,
+semilogy(ebno_dB_10sp, BER_10sp, '-*'), hold on,
+semilogy(ebno_dB_1sp, BER_1sp, '-o'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([4.3, 8])
+ylim([1e-8, 1e-1])
+grid on
+xlabel('Gamma [dB]')
+ylabel('BER')
+legend('50', '20', '10', '1', 'Uncoded')
+
+%% Results for 100 iterations
+ebno_dB_100sp = [4.5,4.6,4.65,4.7:0.02:4.94];
+gamma_100sp = 10.^(ebno_dB_100sp/10)*K/N;
+gamma_dB_100sp = 10*log10(gamma_100sp);
+num_err_packet_100sp = zeros(length(ebno_dB_100sp), 1);
+num_err_bit_100sp = zeros(length(ebno_dB_100sp), 1);
+num_attempt_per_snr_100sp = zeros(length(ebno_dB_100sp), 1);
+
+for i = 1:length(ebno_dB_100sp)
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/blade100/simulation_results_multi_100_', num2str(ebno_dB_100sp(i)), '.txt'), 'r');
+    vec = fscanf(fileID, '%d');
+    fclose(fileID);
+    num_attempt_per_snr_100sp(i) = length(vec);
+    num_err_packet_100sp(i) = length(vec(vec~=0));
+    num_err_bit_100sp(i) = sum(vec(vec~=0));
+end
+
+BER_100sp = num_err_bit_100sp./num_attempt_per_snr_100sp;
+BER_100sp = BER_100sp./K;
+PER_100sp_fromBER = 1 - (1 - BER_100sp).^K;
+
+figure,
+semilogy(ebno_dB_100sp, BER_100sp, '-x'), hold on,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_20sp, BER_20sp, '-d'), hold on,
+semilogy(ebno_dB_10sp, BER_10sp, '-*'), hold on,
+semilogy(ebno_dB_1sp, BER_1sp, '-o'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([4.5, 4.95])
+ylim([5e-8, 2e-2])
+grid on
+xlabel('Gamma [dB]')
+ylabel('BER')
+legend('100', '50', '20', '10', '1', 'Uncoded')
+
+figure,
+semilogy(ebno_dB_100sp, BER_100sp, '-x'), hold on,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_20sp, BER_20sp, '-d'), hold on,
+semilogy(ebno_dB_10sp, BER_10sp, '-*'), hold on,
+semilogy(ebno_dB_1sp, BER_1sp, '-o'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([3.5, 6])
+ylim([5e-8, 2e-2])
+grid on
+xlabel('Gamma [dB]')
+ylabel('BER')
+legend('100', '50', '20', '10', '1', 'Uncoded')
+
+%% Compare with MinSum
+
+
+ebno_dB_minsum =  [2, 2.5, 3, 3.5, 4, 4.5, 4.7, 4.8, 4.9, 5, 5.1, 5.2, 5.3, 5.4, 5.5]; 
 gamma_minsum = 10.^(ebno_dB_minsum/10)*K/N;
 gamma_dB_minsum = 10*log10(gamma_minsum);
 num_err_packet_ms = zeros(length(ebno_dB_minsum), 1);
@@ -35,8 +206,9 @@ num_err_bit_ms = zeros(length(ebno_dB_minsum), 1);
 num_attempt_per_snr_ms = zeros(length(ebno_dB_minsum), 1);
 
 for i = 1:length(ebno_dB_minsum)
-    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/simulation_results_minsum', num2str(ebno_dB_minsum(i)), '.txt'), 'r');
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/bladems50/simulation_results_minsum_', num2str(ebno_dB_minsum(i)), '.txt'), 'r');
     vec = fscanf(fileID, '%d');
+    fclose(fileID);
     num_attempt_per_snr_ms(i) = length(vec);
     num_err_packet_ms(i) = length(vec(vec~=0));
     num_err_bit_ms(i) = sum(vec(vec~=0));
@@ -44,50 +216,64 @@ end
 
 BER_ms = num_err_bit_ms./num_attempt_per_snr_ms;
 BER_ms = BER_ms./K;
-
+PER_ms_fromBER = 1 - (1 - BER_ms).^K;
 PER_ms = num_err_packet_ms./num_attempt_per_snr_ms;
 
+
 figure,
-semilogy(gamma_dB_sumproduct, BER_sp, '-^'), hold on,
-semilogy(gamma_dB_minsum, BER_ms, '-*'), hold on,
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_minsum, BER_ms, '-o'), hold on,
 semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
-xlim([4, 10])
-ylim([5e-8, 3e-1])
+xlim([4, 6])
+ylim([5e-8, 2e-2])
 grid on
-xlabel('Gamma [dB]')
+xlabel('EBN0 [dB]')
 ylabel('BER')
-legend('LDPC, simulation, SumProduct', 'LDPC, simulation, MinSum', 'Uncoded')
+legend('Sum Product', 'Min Sum', 'Uncoded')
 
+
+%% Different slopes
+ebno_dB_ossp = [2,3,4,4.5,4.6,4.7,4.74,4.78,4.8:0.02:4.94];
+gamma_ossp = 10.^(ebno_dB_ossp/10)*K/N;
+gamma_dB_ossp = 10*log10(gamma_ossp);
+num_err_packet_ossp = zeros(length(ebno_dB_ossp), 1);
+num_err_bit_ossp = zeros(length(ebno_dB_ossp), 1);
+num_attempt_per_snr_ossp = zeros(length(ebno_dB_ossp), 1);
+
+for i = 1:length(ebno_dB_ossp)
+    fileID = fopen(strcat('/Users/Michele/Dev/git/channelcoding1516/final_project_DWDM/results/results_blade/bladeos/simulation_results_os100_', num2str(ebno_dB_ossp(i)), '.txt'), 'r');
+    vec = fscanf(fileID, '%d');
+    fclose(fileID);
+    num_attempt_per_snr_ossp(i) = length(vec);
+    num_err_packet_ossp(i) = length(vec(vec~=0));
+    num_err_bit_ossp(i) = sum(vec(vec~=0));
+end
+
+BER_ossp = num_err_bit_ossp./num_attempt_per_snr_ossp;
+BER_ossp = BER_ossp./K;
+PER_ossp_fromBER = 1 - (1 - BER_ossp).^K;
 
 figure,
-semilogy(gamma_dB_sumproduct, PER_sp, '-^'), hold on,
-semilogy(gamma_dB_minsum, PER_ms, '-*'), hold on,
-semilogy(0:10, PER_uncoded(10.^((0:10)/10)), '-v')
-xlim([4, 10])
-ylim([3e-5, 1.05])
+semilogy(ebno_dB_sumproduct, BER_sp, '-^'), hold on,
+semilogy(ebno_dB_ossp, BER_ossp, '-o'), hold on,
+semilogy(0:10, Pbit_uncoded(10.^((0:10)/10)), '-v')
+xlim([4, 5.5])
+ylim([5e-8, 2e-2])
 grid on
-xlabel('Gamma [dB]')
-ylabel('PER')
-legend('LDPC, simulation, SumProduct', 'LDPC, simulation, MinSum', 'Uncoded')
+xlabel('EBN0 [dB]')
+ylabel('BER')
+legend('1,2,3,4,5,6,7', '1,2,3,5,7,11,13', 'Uncoded')
 
+% 
+ebno_sp = [3, 4, 4.5, 4.6, 4.7, 4.8, 4.9, 5];
+time_sp_50 = [6.21475e+09, 6.17265e+09, 6.03251e+09, 5.04901e+09, 2.60614e+09,  1.39746e+09, 1.07678e+09, 9.03046e+08]./1e9;
+time_sp_100 = [1.06808e+10, 1.05435e+10, 1.01902e+10, 7.64292e+09, 2.64309e+09, 1.26275e+09, 9.9787e+08, 9.39012e+08]./1e9;
 
-%% MinSum
-
-time_minsum_n = [1.68831e+09, 1.24743e+09, 7.90332e+08, 5.26343e+08, 3.4851e+08, 1.98934e+08, 1.49833e+08, 1.35503e+08, 1.24461e+08, 1.17984e+08];
-time_minsum = time_minsum_n./(1e9); % in second
-
-time_sumproduct_n = [1.03241e+10, 9.12977e+09, 7.99721e+09, 7.10846e+09, 6.55603e+09, 4.30983e+09, 3.80277e+09, 3.52308e+09, 3.3068e+09];
-time_sumproduct = time_sumproduct_n./(1e9); % in second
-
-
-%% Time
 figure,
-stem(gamma_dB_sumproduct(end-8:end), time_sumproduct, '-^'), hold on,
-stem(gamma_dB_minsum(end-9:end), time_minsum, '-*'), hold on,
+stem(1:length(time_sp_50), time_sp_50, '-^'), hold on,
+stem(1:length(time_sp_100), time_sp_100, '-^'), hold on,
 grid on
-xlabel('Gamma [dB]')
-ylabel('Time to perform a decoding [s]')
-legend('LDPC, SumProduct', 'LDPC, MinSum')
-
-
+xlabel('EBN0 [dB]')
+ylabel('T [s]')
+legend('Sum Product, 50', 'Sum Product, 100')
 
